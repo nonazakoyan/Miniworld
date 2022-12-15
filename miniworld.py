50b53ac7802a705ab722d29c6441151c4f35dc1a
nonazakoyan <61244140+nonazakoyan@users.noreply.github.com>
2022-12-15 23:42:46 +0400

    Update miniworld.py

--- miniworld.py
+++ miniworld.py
@@ -1,45 +1,88 @@
-class Sun:
-    def __init__(self, daytime: int, nigthtime: int):
+from random import randint
+
+class MiniWorld:
+    def __init__(self, daytime, nighttime, oxygen):
         self.day = daytime
-        self.nigth = nigthtime
-
-class Tree:
-    def __init__(self, air: int):
-        self.air = air
-    def foo(self):
-        sun = Sun(10, 10)
-        if sun.day > 0:
-            for i in range(sun.day):
-                self.air += 60
-        print(self.air)
-        
+        self.night = nighttime
+        self.air = oxygen
+    def setday(self, oday):
+        self.day = oday
+    def setnight(self, onight):
+        self.night = onight
+    def setair(self, oair):
+        self.air = oair
+    def getday(self):
+        return self.day
+    def getnight(self):
+        return self.night
+    def getair(self):
+        return self.air
+    
 class Grass:
-    def __init__(self):
-        print("frog eating a grass")
+    def __init__(self, count):
+        self.count = count
+    def setgrass(self, countgrass):
+        self.count = countgrass
+    def getgrass(self):
+        return self.count
 
-class Frog():
-    def __init__(self, move_length,move_width):
-        self.move_length = move_length
-        self.move_width = move_width
-        
+class Tree(MiniWorld):
+    def __init__(self, world):
+        self.air = world.air
+        self.day = world.day
+        self.night = world.night
+    def photosynthesis(self, world):
+        world.air += 40
+        self.setair(world.air)
+        print(f"oxygen - {world.getair()}")
+
+class Frog(MiniWorld):
+    move_left = randint(0, 1)
+    move_right = 1 - move_left
+    def __init__(self, world):
+        self.air = world.air
+        self.day = world.day
+        self.night = world.night
     def move(self):
-        if self.move_length:
-            print("frog move left and right")
-        if self.move_width:
-            print("frog move up and down")
-    def sleep(self):
-        self.move_length = 0
-        self.move_width = 0
-        print("frog is sleep")
-    def awake(self):
-        tree = Tree(20)
-        sun = Sun(10, 10)
-        print("555")
-        if sun.day > 0:
-            for i in range(sun.day):
-                tree.air -= 10
-                if i % 4 == 0:
-                    grass = Grass()
-        
-frog = Frog(0, 5)
-frog.awake()
+        if self.move_left > 0:
+            print("frog move to the left")
+        if self.move_right > 0:
+            print("frog move to the right")
+    def sleep(self, world):
+        world.air -= 10
+        self.setair(world.air)
+        print(f"oxygen - {world.getair()}")
+    def awake(self, world):  
+        world.air -= 20
+        self.setair(world.air)
+        print(f"oxygen - {world.getair()}")
+
+class Program:
+    day = 10
+    night = 10
+    oxygen = 42
+    world = MiniWorld(day, night, oxygen)
+    tree = Tree(world)
+    frog = Frog(world)
+    grass = Grass(100)
+    for week in range(4):
+        i, j = 0, 0
+        while  i < day:
+            tree.photosynthesis(world)
+            frog.awake(world)
+            if i % 2 == 0:
+                frog.move()
+            if i % 3 == 0:
+                grass.setgrass(grass.getgrass() - 1)
+                print(f"frog eating a grass, grass {grass.getgrass()}")
+            i += 1
+
+        while j < night:
+            frog.sleep(world)
+            if j % 2 == 0:
+                grass.setgrass(grass.getgrass() + 1)
+                print(f"grass {grass.getgrass()}")
+            j += 1
+    
+prog = Program()
+prog
